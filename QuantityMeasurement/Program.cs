47318a3d3db9edc8ca3f1a1ed85ac1f29380d85b
priyansh-ankre <bod123priyansh@gmail.com>
2020-08-15 19:24:11 +0530

    [Priyansh]Added:Welcome message and .gitignore file

---
+++ QuantityMeasurement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuantityMeasurement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Quantity Measurement Application!");
+        }
+    }
+}
