90006e3e53ff3cc63447c0c6611e22a26f87b32f
priyansh-ankre <bod123priyansh@gmail.com>
2020-08-18 19:54:39 +0530

    [Priyansh]Refactored:indentations and added documentation

--- QuantityMeasurement/QuantityMeasurementException.cs
+++ QuantityMeasurement/QuantityMeasurementException.cs
@@ -1,19 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="QuantityMeasurementException.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Priyansh"/>
+// --------------------------------------------------------------------------------------------------------------------
 namespace QuantityMeasurement
 {
+    using System;
+
+    /// <summary>
+    /// This Class used to Define Custom Exceptions.
+    /// </summary>
+    /// <seealso cref="System.Exception" />
     public class QuantityMeasurementException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QuantityMeasurementException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="type">The type.</param>
+        public QuantityMeasurementException(string message, ExceptionType type) : base(message)
+        {
+            this.Type = type;
+        }
+
+        /// <summary>
+        /// Enum is Used to define Enumerated Data types
+        /// </summary>
         public enum ExceptionType
         {
+            /// <summary>
+            /// The invalid exception
+            /// </summary>
             INVALID_EXCEPTION
         }
-        public readonly ExceptionType type;
-         public QuantityMeasurementException(string message,ExceptionType type) : base(message)
-        {
-            this.type = type;
-        }
+
+        /// <summary>
+        /// Gets or sets the type.
+        /// </summary>
+        /// <value>
+        /// The type.
+        /// </value>
+        public ExceptionType Type { get; set; }
     }
 }
--- QuantityMeasurement/Unit.cs
+++ QuantityMeasurement/Unit.cs
@@ -1,28 +1,95 @@
-﻿namespace QuantityMeasurement
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Unit.cs" company="Bridgelabz">
+//   Copyright © 2020 Company="BridgeLabz"
+// </copyright>
+// <creator name="Priyansh"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace QuantityMeasurement
 {
+    /// <summary>
+    /// This class contains different quantities
+    /// </summary>
     public class Unit
     {
-        public readonly static Unit FEET = new Unit(12.0);
-        public readonly static Unit INCH = new Unit(1.0);
-        public readonly static Unit YARD = new Unit(36.0);
-        public readonly static Unit CENTIMETER = new Unit(0.4);
-        public readonly static Unit LITRE = new Unit(1.0);
-        public readonly static Unit GALLON = new Unit(3.78);
-        public readonly static Unit MILILITRE = new Unit(0.001);
-        public readonly static Unit KILOGRAM = new Unit(1.0);
-        public readonly static Unit GRAM = new Unit(0.001);
-        public readonly static Unit TONNE = new Unit(1000);
-        public readonly static Unit FAHRENHIET = new Unit(1.0);
-        public readonly static Unit CELCIUS = new Unit(2.12);
+        /// <summary>
+        /// This enum is used for feet with baseUnitConversion value as 12.0
+        /// </summary>
+        public static readonly Unit FEET = new Unit(12.0);
 
-        private double baseUnitConvertor;
+        /// <summary>
+        /// This enum is used for inch with baseUnitConversion value as 1.0
+        /// </summary>
+        public static readonly Unit INCH = new Unit(1.0);
 
+        /// <summary>
+        /// This enum is used for yard with baseUnitConversion value as 36.0
+        /// </summary>
+        public static readonly Unit YARD = new Unit(36.0);
+
+        /// <summary>
+        /// This enum is used for centimeter with baseUnitConversion value as 0.4
+        /// </summary>
+        public static readonly Unit CENTIMETER = new Unit(0.4);
+
+        /// <summary>
+        /// This enum is used for liter with baseUnitConversion value as 1.0
+        /// </summary>
+        public static readonly Unit LITRE = new Unit(1.0);
+
+        /// <summary>
+        /// This enum is used for gallon with baseUnitConversion value as 3.78
+        /// </summary>
+        public static readonly Unit GALLON = new Unit(3.78);
+
+        /// <summary>
+        /// This enum is used for milliliter with baseUnitConversion value as 0.001
+        /// </summary>
+        public static readonly Unit MILILITRE = new Unit(0.001);
+
+        /// <summary>
+        /// This enum is used for kiloGram with baseUnitConversion value as 1.0
+        /// </summary>
+        public static readonly Unit KILOGRAM = new Unit(1.0);
+
+        /// <summary>
+        /// This enum is used for gram with baseUnitConversion value as 0.001
+        /// </summary>
+        public static readonly Unit GRAM = new Unit(0.001);
+
+        /// <summary>
+        /// This enum is used for tone with baseUnitConversion value as 1000
+        /// </summary>
+        public static readonly Unit TONNE = new Unit(1000);
+
+        /// <summary>
+        /// This enum is used for fahrenheit with baseUnitConversion value as 1.0
+        /// </summary>
+        public static readonly Unit FAHRENHIET = new Unit(1.0);
+
+        /// <summary>
+        /// This enum is used for celsius with baseUnitConversion value as 2.12
+        /// </summary>
+        public static readonly Unit CELCIUS = new Unit(2.12);
+
+        /// <summary>
+        /// This value is used when the enum is called 
+        /// </summary>
+        private readonly double baseUnitConvertor;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Unit"/> class.
+        /// </summary>
+        /// <param name="baseUnitConvertor">It contains the value for converting the enum of same type</param>
         private Unit(double baseUnitConvertor)
         {
             this.baseUnitConvertor = baseUnitConvertor;
         }
-            
 
+        /// <summary>
+        /// This method convert the value from one quantity to another quantity of same type
+        /// </summary>
+        /// <param name="value">It contains the value when the method is passed</param>
+        /// <returns>It returns the converted value from one quantity to another quantity of same type</returns>
         public double GetConvertedValue(double value)
         {
             return this.baseUnitConvertor * value;
